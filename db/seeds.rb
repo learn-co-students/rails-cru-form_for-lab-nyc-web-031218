52f72d4b6c3451c030b6b2a62d0a5bca6208eeb4
Dwire <gjdwyer8@gmail.com>
2018-04-09 20:39:49 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# j = Artist.create(name: 'John', bio: 'short little bio')
+# a = Artist.create(name: 'Adam', bio: 'much longer bio')
+# s = Artist.create(name: 'Sam', bio: 'bio's are for kids')
