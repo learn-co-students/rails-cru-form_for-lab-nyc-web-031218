b89e156bd2b9b27e22b5cbb3cbd5029dbf6acd06
Jon <jbvaldesuso@gmail.com>
2018-04-09 23:13:01 -0400

    Done.

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+# require 'rails_helper'
+#
+# RSpec.describe Artist, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
 
-RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+###### I DONT GET THIS
+###### ADD SOME EXAMPLES????
+###### OK.... PLEASE BE MORE SPECIFIC
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+# require 'rails_helper'
+#
+# RSpec.describe Song, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
 
-RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+###### I DONT GET THIS
+###### ADD SOME EXAMPLES????
+###### OK.... PLEASE BE MORE SPECIF
