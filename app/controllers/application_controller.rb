bed7b20f43cc53eb26d76e6f4f8a7eec54c66a90
harim1206 <harim1206@gmail.com>
2018-04-10 18:22:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+# rails g resource Artist name:string bio:string --no-test-framework
+# rails g resource Genre name:string --no-test-framework
+# rails g resource Song name:string artist_id:integer genre_id:integer --no-test-framework
+#
+#
+# table "artists"
+#   string   "name"
+#   text     "bio"
+#
+# table "genres"
+#   string   "name"
+#
+# table "songs"
+#   string   "name"
+#   integer  "artist_id"
+#   integer  "genre_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Artist.create(name:"Terekke", bio:"Lofi from Holland")
+Artist.create(name:"DJ Harvey", bio:"Disco Legend")
+Artist.create(name:"Nils Frahm", bio:"Pianist")
+
+Genre.create(name:"Disco")
+Genre.create(name:"House")
+Genre.create(name:"Afrobeat")
